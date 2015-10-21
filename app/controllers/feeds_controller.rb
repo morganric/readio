b2ffe497a66cbf37c04e4e36eed767a8c1b0e4fe
morganric <morganric@gmail.com>
2015-10-21 16:37:23 +0100

    fix

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,6 +2,7 @@ class FeedsController < ApplicationController
   before_action :set_feed, only: [:show, :edit, :update, :destroy]
 
   require 'rss'
+  include FeedsHelper
 
   # GET /feeds
   # GET /feeds.json
@@ -13,29 +14,7 @@ class FeedsController < ApplicationController
   # GET /feeds/1.json
   def show
 
-
-    @items = RSS::Parser.parse(open(@feed.url).read, false).items
-
-    @items.each do |item|
-
-        if Date.parse(item.pubDate.strftime("%Y-%m-%d %I:%M%P")) > Date.parse(@items.first.pubDate.strftime("%Y-%m-%d %I:%M%P")) ||  @feed.items == []
-          i = Item.new
-          i.title = item.title
-          i.description = item.description
-          i.pub_date = item.pubDate
-          i.link = item.link
-          i.title = item.title
-          i.url = item.enclosure.url
-          i.guid = item.guid
-          i.author = item.author
-          i.plays = rand(1..100)
-          i.feed_id = @feed.id
-
-          i.save
-
-          end
-
-      end
+    update_feed(@feed)
 
       @items = @feed.items.page params[:page]
 
--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,2 +1,32 @@
 module FeedsHelper
+
+
+	def update_feed(feed)
+
+
+    @items = RSS::Parser.parse(open(feed.url).read, false).items
+
+    @items.each do |item|
+
+        if Date.parse(item.pubDate.strftime("%Y-%m-%d %I:%M%P")) > Date.parse(@items.first.pubDate.strftime("%Y-%m-%d %I:%M%P")) ||  @feed.items == []
+          i = Item.new
+          i.title = item.title
+          i.description = item.description
+          i.pub_date = item.pubDate
+          i.link = item.link
+          i.title = item.title
+          i.url = item.enclosure.url
+          i.guid = item.guid
+          i.author = item.author
+          i.plays = rand(1..100)
+          i.feed_id = @feed.id
+
+          i.save
+
+          end
+
+      end
+
+	end
+
 end
